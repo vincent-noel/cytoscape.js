02c69d043382361899252112f76e737707af3aad
metincansiper <metincansiper@gmail.com>
2016-11-30 12:19:55 +0200

    preparing to publish 2.7.11.3

--- dist/cytoscape.js
+++ dist/cytoscape.js
@@ -2,7 +2,7 @@
 
 /*!
 
-Cytoscape.js 2.7.11.2 (MIT licensed)
+Cytoscape.js 2.7.11.3 (MIT licensed)
 
 Copyright (c) The Cytoscape Consortium
 
@@ -3179,7 +3179,7 @@ var boundingBoxImpl = function( ele, options ){
       // handle node dimensions
       /////////////////////////
       
-      var stateAndInfos = ele._private.data.sbgnstatesandinfos;
+      var stateAndInfos = ele._private.data.statesandinfos;
       var ports = ele._private.data.ports;
       var minY, maxY, minX, maxX;
       
@@ -27500,7 +27500,7 @@ util.debounce = function( func, wait, options ){ // ported lodash debounce funct
 module.exports = util;
 
 },{"../is":83,"../window":108}],107:[function(_dereq_,module,exports){
-module.exports="2.7.11.2"
+module.exports="2.7.11.3"
 },{}],108:[function(_dereq_,module,exports){
 module.exports = ( typeof window === 'undefined' ? null : window ); // eslint-disable-line no-undef
 
